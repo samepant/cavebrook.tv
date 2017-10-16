978ca0ad26c47f9acf3662fe105dbb5294bdf5dc
samepant <sampanter@gmail.com>
2017-10-16 10:27:17 -0400

    fixes caht url

--- src/components/livechat.js
+++ src/components/livechat.js
@@ -6,7 +6,7 @@ export default class extends React.Component {
       <div className='liveBox'>
         <div className='liveContainer'>
           <iframe id='ytchat' title='chat' type='text/html' 
-            src='https://www.youtube.com/live_chat?v=LH0VK2CYQyE&embed_domain=cavebrook.tv'
+            src='https://www.youtube.com/live_chat?v=n9NQib9dQBs&embed_domain=cavebrook.tv'
             frameBorder="0"></iframe>
         </div>
       </div>
